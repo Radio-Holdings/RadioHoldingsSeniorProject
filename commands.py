3c0b156cafe66b38939668a67b7b8800697b5d9d
Lucas Alvarez <lucasalvarez@pm.me>
2022-07-28 10:21:54 -0400

    Update with commands

--- commands.py
+++ commands.py
@@ -22,38 +22,46 @@ import os
     SYSTEM TIME
     *81
 """
+
+
 def connect_to_node(node_number):
     print(f"Connecting to node {node_number}.")
-    command = "sudo asterisk -rx \"rpt fun 1999 *3{}\"".format(node_number)
+    command = 'sudo asterisk -rx "rpt fun 1999 *3{}"'.format(node_number)
     print("Command executed: ", command)
     os.system(command)
+
 
 def disconnect_from_all_nodes():
     print(f"Disconnecting from all nodes.")
-    command = "sudo asterisk -rx \"rpt fun 1999 *71\""
+    command = 'sudo asterisk -rx "rpt fun 1999 *71"'
     print("Command executed: ", command)
     os.system(command)
+
 
 def disconnect_from_node(node_number):
     print(f"Disconnecting from node {node_number}.")
-    command = "sudo asterisk -rx \"rpt fun 1999 *1{}\"".format(node_number)
+    command = 'sudo asterisk -rx "rpt fun 1999 *1{}"'.format(node_number)
     print("Command executed: ", command)
     os.system(command)
+
 
 def announce_connection_status():
     print("Announcing connection status")
-    command = "sudo asterisk -rx \"rpt fun 1999 *73\""
+    command = 'sudo asterisk -rx "rpt fun 1999 *73"'
     print("Command executed: ", command)
     os.system(command)
+
 
 def announce_system_time():
     print("Announcing system time")
-    command = "sudo asterisk -rx \"rpt fun 1999 *81\""
+    command = 'sudo asterisk -rx "rpt fun 1999 *81"'
     print("Command executed: ", command)
     os.system(command)
 
+
 def reconnect_all_previous_links():
     print("reconnecting all previous links")
-    command = "sudo asterisk -rx \"rpt fun 1999 *74\""
+    command = 'sudo asterisk -rx "rpt fun 1999 *74"'
     print("Command executed: ", command)
     os.system(command)
+
